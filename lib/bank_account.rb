dd08b8389dffa0690d87020089f5a104ab5e033b
allengoh <allen1311@hotmail.com>
2022-09-22 16:37:25 +0100

    Test drive the bank account class

---
+++ lib/bank_account.rb
@@ -0,0 +1,47 @@
+require_relative "./transactions"
+
+class BankAccount
+
+  attr_reader :balance
+
+  def initialize(transactions_class = Transactions.new())
+    @balance = 0
+    @transactions = transactions_class
+  end
+  
+  def deposit(date, amount)
+    @balance += amount
+    @transactions.add_transaction(date, amount, "", @balance)
+  end
+
+  def withdraw(date, amount)
+    @balance -= amount
+    @transactions.add_transaction(date, "", amount, @balance)
+  end
+
+  def print_statement(transactions = @transactions)
+    if transactions.all.empty?
+      return "There are no transactions to print out"
+    else
+      puts formatter
+      transactions.all.reverse.each do |transaction|
+        puts "#{transaction.date} || #{transaction.credit} || #{transaction.debit} || #{transaction.balance}"  
+      end
+    end
+  end
+  
+
+  private 
+
+  def formatter 
+    return "date || credit || debit || balance"
+  end
+
+end
+
+
+
+
+
+
+
---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,37 @@
+require "bank_account"
+
+RSpec.describe BankAccount do
+
+  it "initialize with a balance of 0" do 
+    bank_account = BankAccount.new()
+    expect(bank_account.balance).to eq(0)
+  end
+
+  it "returns a balance of 500.00 with a deposit transaction" do 
+    bank_account = BankAccount.new()
+    bank_account.deposit("17/09/22", 500.00)
+    expect(bank_account.balance).to eq(500.00)
+  end
+
+  it "returns a balance of 100.00 with a deposit and a withdrawal transaction" do 
+    bank_account = BankAccount.new()
+    bank_account.deposit("17/09/22", 500.00)
+    bank_account.withdraw("18/09/22", 300.00)
+    expect(bank_account.balance).to eq(200.00)
+  end
+
+  it "returns a no transaction to print out statement if there are no transactions" do 
+    bank_account = BankAccount.new()
+    expect(bank_account.print_statement()).to eq("There are no transactions to print out")    
+  end
+
+  it "prints out a statement when there is a transaction" do 
+    bank_account = BankAccount.new()
+    bank_account.deposit("17/09/22", 500.00)
+    bank_account.withdraw("18/09/22", 300.00)
+    expect(bank_account.print_statement()[0].debit).to eq(300.00)    
+    expect(bank_account.print_statement()[1].credit).to eq(500.00)    
+    expect(bank_account.print_statement()[0].balance).to eq(200.00)    
+  end
+
+end
